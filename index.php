613b38bbf6e10d1efb74fec6b67a30b64bb8c82f
cellum95 <101460624+cellum95@users.noreply.github.com>
2022-03-12 22:51:54 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    
+    <meta charset = "utf-8">
+    <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+    <title>Color Table</title>
+    <meta name = "description" content = "A webpage to store colors in PHP Session">
+    <link href = "CSS/style.php" rel = "stylesheet" type = "text/css">
+    
+</head>
+<body>
+
+	<header class = "header-title">
+		<h1>Looping Experiment</h1>
+	</header>
+	
+	<main>
+		<h2>Color Table</h2>
+		
+		<table class = "color-table">
+			<tr>
+				<th colspan = "2">Color Table</th>
+			</tr>
+			<tr>
+				<td>Favorite Color: </td>
+				<td></td>
+				
+			</tr>
+			<tr>
+				<td>Favorite Color: </td>
+				<td></td>
+				
+			</tr>
+			<tr>
+				<td>Favorite Color: </td>
+				<td></td>
+				
+			</tr>
+			<tr>
+				<td>Favorite Color: </td>
+				<td></td>
+			
+			</tr>
+		</table>
+	</main>
+</body>
+</html>
