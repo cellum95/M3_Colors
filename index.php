aeddc5f7eb982cd3303e29baaa77c7970237cf9b
cellum95 <101460624+cellum95@users.noreply.github.com>
2022-03-13 17:47:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
 <!DOCTYPE html>
 
 <html>
-<head>
-    
-    <meta charset = "utf-8">
-    <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
-    <title>Color Table</title>
-    <meta name = "description" content = "A webpage to store colors in PHP Session">
-    <link href = "CSS/style.php" rel = "stylesheet" type = "text/css">
-    
-</head>
+	<head>
+    		<meta charset = "utf-8">
+    		<meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+    		<title>Color Table</title>
+    		<meta name = "description" content = "A webpage to store colors in PHP Session">
+    		<link href = "CSS/style.php" rel = "stylesheet" type = "text/css">
+	</head>
 <body>
 
 	<header class = "header-title">
