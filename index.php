b0b2b60b6ca92ae6f16035bd597281e2d8f735eb
cellum95 <101460624+cellum95@users.noreply.github.com>
2022-03-13 17:50:10 +0800

    Update index.php

--- index.php
+++ index.php
@@ -15,38 +15,38 @@
 		</header>
 	
 		<main>
-		<form action = "Pages/Second_Page.php" method = "post">
-			<h2>Color Table</h2>
-		
-				<table class = "color-table">
-					<tr>
-						<th colspan = "2">Color Table</th>
-					</tr>
-					<tr>
-						<td>Favorite Color: </td>
-						<td><input type = "text" name = "Fav_Color_1" style = "width : 90%;"></td>
-					</tr>
-					<tr>
-						<td>Favorite Color: </td>
-						<td><input type = "text" name = "Fav_Color_2" style = "width : 90%;"></td>	
-					</tr>
-					<tr>
-						<td>Favorite Color: </td>
-						<td><input type = "text" name = "Fav_Color_3" style = "width : 90%;"></td>
-					</tr>
-					<tr>
-						<td>Favorite Color: </td>
-						<td><input type = "text" name = "Fav_Color_4" style = "width : 90%;"></td>
-					</tr>
-					<tr>
-						<td>Favorite Color: </td>
-						<td><input type = "text" name = "Fav_Color_5" style = "width : 90%;"></td>
-					</tr>
-					<tr>
-						<td colspan = "2"><input type = "submit"></td>
-					</tr>
-				</table>
-			</form>
+			<form action = "Pages/Second_Page.php" method = "post">
+				<h2>Color Table</h2>
+
+					<table class = "color-table">
+						<tr>
+							<th colspan = "2">Color Table</th>
+						</tr>
+						<tr>
+							<td>Favorite Color: </td>
+							<td><input type = "text" name = "Fav_Color_1" style = "width : 90%;"></td>
+						</tr>
+						<tr>
+							<td>Favorite Color: </td>
+							<td><input type = "text" name = "Fav_Color_2" style = "width : 90%;"></td>	
+						</tr>
+						<tr>
+							<td>Favorite Color: </td>
+							<td><input type = "text" name = "Fav_Color_3" style = "width : 90%;"></td>
+						</tr>
+						<tr>
+							<td>Favorite Color: </td>
+							<td><input type = "text" name = "Fav_Color_4" style = "width : 90%;"></td>
+						</tr>
+						<tr>
+							<td>Favorite Color: </td>
+							<td><input type = "text" name = "Fav_Color_5" style = "width : 90%;"></td>
+						</tr>
+						<tr>
+							<td colspan = "2"><input type = "submit"></td>
+						</tr>
+					</table>
+				</form>
 		
 		</main>
 	</body>
